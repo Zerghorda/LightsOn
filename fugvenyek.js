7af66609acda9665a51d6bb15b568a57b42b0bf6
Zerghorda21 <tar.gergo21@gmail.com>
2024-06-05 13:30:32 +0200

    kinézet fix,event valamit csinál,PALYA fv gen kell

--- fugvenyek.js
+++ fugvenyek.js
@@ -5,39 +5,37 @@ export function megjelenit(txt, element) {
 }
 export function palyaOsszeallit(meret) {
   let txt = "";
-  txt += '<div class="row">';
-  for (let sor = 0; sor < meret; sor++) {
-    txt += '<div clas="col-lg-6">';
-    for (let oszlop = 0; oszlop < meret; oszlop++) {
-      let felVLe = Math.floor(Math.random() * 2);
-      if (felVLe === 1) {
-        txt += `<span id="${sor},${oszlop}" class="felkapcsolt"></span>`;
-      } else {
-        txt += `<span id="${sor},${oszlop}" class="lekapcsolt"></span>`;
-      }
+  txt += `<div class="row">`;
+  for (let i = 0; i < meret * meret; i++) {
+    txt += `<div class="col-4 lampa">`;
+    let felVLe = Math.random();
+    if  (felVLe >= 0.2) {
+      txt += `<span  id="${i}"  class="felkapcsolt"></span>`;
+
+    } else {
+      txt += `<span  id="${i}"  class="lekapcsolt"></span>`;
     }
-    txt += "</div>";
+    txt += `</div>`;
   }
-  txt += "</div>";
+  txt += `</div>`;
   return txt;
 }
 export function hagyLekapcsolva(txt) {
   let db = txt.match(/lekapcsolt/g).length;
-  console.log(db);
   return db;
 }
 
 export function kapcsolas() {
-  const lampaELEM = $("article span") 
-  lampaELEM.on("click",function(event) {
+  const spanELEM = $(".lampa span");
+  spanELEM.on("click", function (event) {
     let index = event.target.id;
     console.log(index);
     if (index.includes("felkapcsolt")) {
-      index.replace("felkapcsolt","lekapcsolt")
+      index.replace("felkapcsolt", "lekapcsolt");
     }
     if (index.includes("lekapcsolt")) {
-      index.replace("lekapcsolt","felkapcsolt")
+      index.replace("lekapcsolt", "felkapcsolt");
     }
-  })
-  init()
+  });
+ 
 }
--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-import { megjelenit, palyaOsszeallit, hagyLekapcsolva } from "./fugvenyek.js";
+import {
+  megjelenit,
+  palyaOsszeallit,
+  hagyLekapcsolva,
+  kapcsolas,
+} from "./fugvenyek.js";
 const articleELEM = $("article");
 const asideLeirasELEM = $("#leiras");
 
 let meret = 3;
+const PALYA = [];
+//function listaTolt(PALYA) {
+// let txt = palyaOsszeallit(meret);
+//for (let index = 0; index < meret * meret; index++) {
+//if (txt.match("lekapcsolt")) {
+
+//}
+//}
+//}
 
 init();
 
 export function init() {
   let txt = palyaOsszeallit(meret);
   megjelenit(txt, articleELEM);
+  kapcsolas()
   let db = hagyLekapcsolva(txt);
   megjelenit(
     `<p>Kapcsold le a lehető<br> legtöbb lámpát!(Legyen az összes zöld!)<br>A lekapcsolt lámpák száma:<br><span id="lekapcsoltakSz">${db}</span></p>`,
